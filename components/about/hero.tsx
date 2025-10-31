2cb71cd8ff2e5f8a75e900db1cdb07d93ac11710
sentack <sentak16@gmail.com>
2025-10-31 18:16:47 +0300

    type error fixed

--- components/about/hero.tsx
+++ components/about/hero.tsx
@@ -24,8 +24,8 @@ export function AboutHero() {
     hidden: { opacity: 0, y: 20 },
     visible: {
       opacity: 1,
-      y: 0,
-      transition: { duration: 0.5, ease: ("easeOut" as unknown) as any }},
+      y: 0,      
+      transition: { duration: 0.8, transition: { duration: 0.5, ease: ("easeOut" as unknown) as any }},
     },
   }
 
